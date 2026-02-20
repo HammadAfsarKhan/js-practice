61444a2b9bc32ba4b1ba7bad8d1c252b971a4161
HammadAfsarKhan <hammadafsar270@gmail.com>
2026-02-20 21:46:48 +0500

    My first javascript practice session

---
+++ app.js
@@ -0,0 +1,53 @@
+//chapter 1 Alerting a message
+
+//question 1
+
+//alert("Hello World!");
+
+//question 2
+
+//alert("error: something went wrong!");
+
+//question 3
+
+//alert("Welcome to JS Land...\nHappy Coding!");
+
+//question 4
+//alert("Welcome to JS Land...");
+//alert("Happy Coding!");
+
+//question 5
+
+//alert("Hello... I can run JS through my web browser's console");
+
+//chapter 2 Variables for Strings
+//question 1
+
+//var username;
+
+//question 2
+
+//var myName = "Jumbo Computers";
+
+//console.log(myName);
+
+//question 3
+
+//var message = "Hello World!";
+//alert(message);
+//console.log(message);
+
+//question 4
+
+//var name = "Jumbo Computers";
+//var age = "1 year";
+//var course = "Certified Mobile Application Development";
+
+//console.log(name);
+//console.log(age);
+//console.log(course);
+
+
+
+
+
