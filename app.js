0714ffc2618add8c4c626ef998d0d6b72bbfc7cf
HammadAfsarKhan <hammadafsar270@gmail.com>
2026-02-21 08:25:35 +0500

    updated app.js

--- app.js
+++ app.js
@@ -47,7 +47,98 @@
 //console.log(age);
 //console.log(course);
 
+//questiion 5
 
+//var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(pizza); 
 
+//chapter 3 Variables for Numbers
+
+//question 1
+//var age = 25;
+//alert("I am " + age + " years old");
+
+//question 2
+//var visitCount = 14;
+//alert("You have visited this site " + visitCount + " times"); 
+
+//question 3
+//var birthYear = 1998;
+//document.write("My birth year is " + birthYear + "<br>");
+//document.write("Data type of my declared variable is number");
+
+//question 4
+//var visitorName = "John Doe";
+//var productTitle = "T-shirt";
+//var quantity = 5;
+//document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+//chapter 4 Variable Names: Legal and Illegal
+
+//question 1
+//var legalVariable;
+//var _legalVariable;
+//var $legalVariable;
+//var legalVariable123;
+//var legal_Variable;
+//question 2
+//var 1illegalVariable;
+//var illegal-Variable;
+//var illegal Variable;
+//var illegal.Variable;
+//var illegalVariable!; 
+
+//question 3
+//document.write("<h1>Rules for naming JS variables</h1>");
+//document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable <br>");
+//document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name <br>");
+//document.write("Variable names are case sensitive <br>");
+//document.write("Variable names should not be JS keywords");
+
+//chapter 5 Math Expressions
+
+//question 1
+//var num1 = 3;
+//var num2 = 5;
+//var sum = num1 + num2;
+//document.write("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+//question 2
+//var num1 = 3;
+//var num2 = 5;
+//var difference = num1 - num2;
+//document.write("The difference of " + num1 + " and " + num2 + " is " + difference);
+
+//question 3
+//var num1 = 3;
+//var num2 = 5;
+//var product = num1 * num2;
+//document.write("The product of " + num1 + " and " + num2 + " is " + product);
+
+//question 4
+//var num1 = 3;
+//var num2 = 5;
+//var quotient = num1 / num2;
+//document.write("The quotient of " + num1 + " and " + num2 + " is " + quotient);
+
+//question 5
+//var num = 5;
+//var incrementedNum = ++num;
+//document.write("The value after increment is: " + incrementedNum + "<br>");
+//var decrementedNum = --num;
+//document.write("The value after decrement is: " + decrementedNum + "<br>");
+//var remainder = num % 3;
+//document.write("The remainder is: " + remainder);
+
+//question 6
+//var ticketPrice = 600;
+//var totalCost = ticketPrice * 5;
+//document.write("Total cost to buy 5 tickets to a movie is " + totalCost + " PKR");
+
+//question 7
+//document.write("Table of 4 <br>");
+//for (var i = 1; i <= 10; i++) {
+//    document.write("4 x " + i + " = " + (4 * i) + "<br>");
+//}
 
 
